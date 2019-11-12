28e4ad2921c070b84d00fc6938d8bfe76e8da953
hudafauzanm <32631105+hudafauzanm@users.noreply.github.com>
2019-11-12 21:21:41 +0700

    Update last

--- Challenge20/Program.cs
+++ Challenge20/Program.cs
@@ -2,6 +2,14 @@ using System;
 
 namespace ObjectClass
 {
+    class Item {
+                public string name;
+                public int price;
+                public bool onSale;
+
+                
+            }
+            
     class Program
     {
         static void Main(string[] args)
@@ -11,6 +19,16 @@ namespace ObjectClass
             item.name = "Indomie Goreng";
             item.price = 3500;
             item.onSale = true; // If true the price will drop 20%
+
+            if (item.onSale == true)
+            {
+                double diskon = item.price*0.2;
+                Console.WriteLine(diskon);
+            }
+
+            string print(){
+                Console.WriteLine(item.name);
+            }
 
             item.print(); // Output: "Indomie Goreng (Rp2800)"
         }
